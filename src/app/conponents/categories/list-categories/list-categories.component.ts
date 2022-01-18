9680777403ad4bccc2dbd5297bd8f063badf4b2f
Ricardo <rmvnew@gmail.com>
2022-01-18 13:04:42 -0400

    added button which navigate to create category

--- src/app/conponents/categories/list-categories/list-categories.component.ts
+++ src/app/conponents/categories/list-categories/list-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-categories',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCategoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  redirect(){
+    this.route.navigateByUrl('/category/create')
   }
 
 }
